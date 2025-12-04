21faba16ffcfd6323444687ae1729253a2f0f068
BicoSteve101 <steve.bico@giktek.io>
2025-12-04 09:38:02 +0300

    Update SpringDoc GroupedOpenApi import to use models package

--- zula-swagger-library/src/main/java/com/zula/swagger/config/SwaggerAutoConfig.java
+++ zula-swagger-library/src/main/java/com/zula/swagger/config/SwaggerAutoConfig.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
