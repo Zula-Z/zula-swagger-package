a5d96409224cdc08ac3635abcd54cebfadf193b2
BicoSteve101 <steve.bico@giktek.io>
2025-12-05 17:10:16 +0300

    Bump version to 1.1.10 and enhance Swagger security configuration with additional endpoints and highest precedence

--- zula-swagger-library/src/main/java/com/zula/swagger/config/SwaggerSecurityAutoConfig.java
+++ zula-swagger-library/src/main/java/com/zula/swagger/config/SwaggerSecurityAutoConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,20 +18,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @ConditionalOnClass({SecurityFilterChain.class, HttpSecurity.class})
-@ConditionalOnProperty(name = "zula.swagger.security.enabled", havingValue = "true", matchIfMissing = true)
-public class SwaggerSecurityAutoConfig {
+    @ConditionalOnProperty(name = "zula.swagger.security.enabled", havingValue = "true", matchIfMissing = true)
+    public class SwaggerSecurityAutoConfig {
 
     private static final String[] SWAGGER_WHITELIST = {
             "/",
             "/error",
             "/swagger-ui/**",
             "/swagger-ui.html",
+            "/swagger-ui/index.html",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/webjars/**",
             "/v3/api-docs/**",
             "/actuator/**"
     };
 
     @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER - 1)
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .securityMatchers(matchers -> matchers.requestMatchers(SWAGGER_WHITELIST))
